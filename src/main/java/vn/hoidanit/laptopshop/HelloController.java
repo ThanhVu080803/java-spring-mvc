578e28f9f282253d4c9c82577cc7d53a0413c2ea
thanhvu080803@gmail.com <thanhvu080803@gmail.com>
2024-07-17 20:36:58 +0700

    update 17 c7

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
 	public String index() {
-		return "Hello world with Thanh Vu!";
+		return "Hello world Thanh Vu!";
+	}
+
+	@GetMapping("/user")
+	public String user() {
+		return "Only user login";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "Only admin login";
 	}
 }
